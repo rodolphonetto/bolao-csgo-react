4d55ab730167f12578425b26187ba8b738f0e47b
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-01-24 16:35:42 -0200

    instalação do redux

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
 import React, { Component } from "react";
 import Landing from "./Components/Layout/Landing";
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
+const store = createStore(() => {
+  [], {}, applyMiddleware();
+});
 
 class App extends Component {
   render() {
     return (
-      <Router>
-        <div className="App">
-          <Route path="/" component={Landing} />
-        </div>
-      </Router>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Route path="/" component={Landing} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
