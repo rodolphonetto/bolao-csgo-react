d99a3772f059081a3be05ac2741b1bda62ad9ad8
Rodolpho Netto <rodolpho.netto@gmail.com>
2019-02-03 00:02:25 -0200

    teste ssh

---
+++ src/teste excluir/store.js
@@ -0,0 +1,21 @@
+import authReducer from "./store/reducers/auth";
+import signupReducer from "./store/reducers/signup";
+import countryReducer from "./store/reducers/country";
+import thunk from "redux-thunk";
+
+import { createStore, combineReducers, applyMiddleware, compose } from "redux";
+
+const rootReducer = combineReducers({
+  auth: authReducer,
+  signup: signupReducer,
+  country: countryReducer
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(thunk))
+);
+
+export default store;
